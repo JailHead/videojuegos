dab2d0b2c15d0024df96ff80de2a15509194822e
JailHead <2122300432@soy.utj.edu.mx>
2024-02-22 18:46:50 -0600

    Append template from out

--- src/app/videojuegos/videojuegos.component.ts
+++ src/app/videojuegos/videojuegos.component.ts
@@ -1,26 +1,41 @@
 import { Component } from "@angular/core";
 @Component({
     selector: 'videojuegos',
-    template: `
-        <h2>Componente de {{nombre}}
-        <h3 *ngIf="mostrar_retro !== true">El mejor juego es {{mejor_juego}}</h3>
-        <h3 [style.background]="'yellow'" *ngIf="mostrar_retro">El juego retro: {{mejor_juego_retro}}</h3>
-        <h2>Listado de videojuegos</h2>
-        <ul>
-            <li *ngFor="let game of videojuegos">{{game}}</li>
-        </ul>
-    `
+    templateUrl: './videojuegos.component.html'
 })
 export class VideojuegosComponent{
-    public nombre='Videojuegos 2024';
-    public mejor_juego = 'GTA 5';
-    public mejor_juego_retro = 'Super Mario Bros';
-    public mostrar_retro = true;
-    public videojuegos = [
-        'Los simpson',
-        'Assasin\'s Creed',
-        'Red Dead Redemption',        
-        'God of War',
-        'Dark Souls',
-    ];
+    public nombre: String;
+    public mejor_juego: String;
+    public mejor_juego_retro: String;
+    public mostrar_retro: Boolean;
+    public year: Number;
+
+    public videojuegos: Array<String>;
+
+    constructor (){
+        this.nombre = 'videojuegos 2024';
+        this.mejor_juego = 'GTA 5';
+        this.mejor_juego_retro = 'Super Mario Bros';
+        this.mostrar_retro = true;
+        this.year = 2024;
+        this.videojuegos = [
+            'Los simpson',
+            'Assasin\'s Creed',
+            'Red Dead Redemption',        
+            'God of War',
+            'Dark Souls'
+        ]
+    }
+
+    // public nombre='Videojuegos 2024';
+    // public mejor_juego = 'GTA 5';
+    // public mejor_juego_retro = 'Super Mario Bros';
+    // public mostrar_retro = true;
+    // public videojuegos = [
+    //     'Los simpson',
+    //     'Assasin\'s Creed',
+    //     'Red Dead Redemption',        
+    //     'God of War',
+    //     'Dark Souls',
+    // ];
 }
